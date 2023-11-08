01c459599488e3bfa45913b1f7ccd78a314374bc
LastWinter <neretin-205@bk.ru>
2023-11-08 15:22:35 +0300

    стартДобавить .gitattributes и .gitignore.

---
+++ lab 2.1 - 1/shablon.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void sayHello() {
+	setlocale(LC_ALL, "RU");
+
+	cout << "Привет" << endl;
+}
+void sayGoodbye() {
+	cout << "Пока" << endl;
+}
+int main() {
+	sayHello();
+	cout << "**********************************" << endl;
+	sayGoodbye();
+	
+
+	return 0;
+}
+
+
+
